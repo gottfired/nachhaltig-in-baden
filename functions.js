89bc99c9d480711b14162aefed22bfaebe5d226d
NINJA\gottfried.chen <gottfried@chengine.com>
2019-01-20 18:07:38 +0100

    refactor navbar into functions.js

--- functions.js
+++ functions.js
@@ -38,3 +38,45 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+document.getElementById("navMenu").innerHTML = `
+  <div class="topnav" id="myTopnav">
+    <a id="navStart" href="index.html">Start</a>
+    <a id="navCook" href="MonatlichesKochrezept.html">Monatliches Kochrezept</a>
+    <a id="navWeekly" href="WoechentlicherTipp.html">Wöchentlicher Tipp</a>
+    <a id="navFood" href="Essen.html">Essen</a>
+    <div class="dropdown">
+      <button onclick="Technologie()" class="dropbtn">Technologie
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div id="Technologie" class="dropdown-content">
+        <a id="navEnergy" href="Energie.html">Energie</a>
+        <a id="navTraffic" href="Verkehr.html">Verkehr</a>
+        <a id="navCo2" href="CO2Komp.html">CO<sub>2</sub> Kompensation</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <button onclick="OnlineHandel()" class="dropbtn">Überregional
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div id="OnlineHandel" class="dropdown-content">
+        <a id="navOnlineShopping" href="Online-Handel.html">Online Handel</a>
+        <a id="navWool" href="Wolle.html">Wolle</a>
+        <a id="navZeroWaste" href="ZeroWaste.html">Zero Waste</a>
+        <a id="navMiscShopping" href="divGesch.html">diverse Geschäfte</a>
+      </div>
+    </div>
+    <a id="navReuse" href="Weiterverwenden.html">Weiterverwenden</a>
+
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+
+  </div>
+`;
+
+function setActive(elementId) {
+    const element = document.getElementById(elementId);
+    element.className += "active";
+}
